44e5be09e80d399e08b232ab6ed8fbb69c35421a
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-28 04:06:39 +0530

    edit profile

--- screens/user/dashboard/dashboard-screen.js
+++ screens/user/dashboard/dashboard-screen.js
@@ -1,9 +1,8 @@
-import React, {useContext, useEffect} from 'react'
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native'
-import {heightPercentageToDP as hp, widthPercentageToDP as wp} from 'react-native-responsive-screen'
+import React, {useContext, useEffect, useState} from 'react'
+import {Button, Image, Platform, StyleSheet, View} from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
 import axios from 'axios'
 import {AppContext} from '../../../global/app-context'
-import Colors from '../../../shared/colors'
 import Menu from '../../../components/buttons/menu-button'
 import CombinedButtons from '../../../components/buttons/combined-buttons'
 import {getStringData} from '../../../helpers/local-storage-helpers'
@@ -11,6 +10,8 @@ import {Util} from '../../../util/util'
 
 const DashboardScreen = () => {
   const appContext = useContext(AppContext)
+
+  const [image, setImage] = useState(null)
 
   useEffect(() => {
     getStringData(Util.EXPO_PUSH_TOKEN).then(value => {
@@ -26,30 +27,78 @@ const DashboardScreen = () => {
     })
   }, [])
 
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync()
+        if (status !== 'granted')
+          alert('Sorry, we need camera roll permissions to make this work!')
+      }
+    })()
+  }, [])
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    })
+
+    console.log(result)
+
+    if (!result.cancelled)
+      // noinspection JSUnresolvedVariable
+      setImage(result.uri)
+  }
+
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={styles.mainViewStyle}>
-          <View style={styles.viewStyle}>
-            <Text>
-              Dashboard
-            </Text>
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.viewStyle}>
+      <Button title='Pick an image from camera roll'
+              onPress={pickImage}/>
+      {
+        image && (
+          <Image style={styles.imageStyle}
+                 source={{
+                   uri: image
+                 }}/>
+        )
+      }
+    </View>
   )
+
+// return (
+//   <SafeAreaView>
+//     <ScrollView>
+//       <View style={styles.mainViewStyle}>
+//         <View style={styles.viewStyle}>
+//           <Text>
+//             Dashboard
+//           </Text>
+//         </View>
+//       </View>
+//     </ScrollView>
+//   </SafeAreaView>
+// )
 }
 
 const styles = StyleSheet.create({
-  mainViewStyle: {
-    width: wp('100%'),
-    height: hp('100%'),
-    backgroundColor: Colors.secondaryColor
+  imageStyle: {
+    width: 200,
+    height: 200
   },
+  // mainViewStyle: {
+  //   width: wp('100%'),
+  //   height: hp('100%'),
+  //   backgroundColor: Colors.secondaryColor
+  // },
   viewStyle: {
+    flex: 1,
     alignItems: 'center',
-    marginTop: 10
+    justifyContent: 'center',
+    // alignItems: 'center',
+    // marginTop: 10
   }
 })
 
