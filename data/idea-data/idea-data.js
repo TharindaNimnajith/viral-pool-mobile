cfff5cae2dd950448a17de4596474eeb0f5a1b8e
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-27 14:50:33 +0530

    fix

--- data/idea-data/idea-data.js
+++ data/idea-data/idea-data.js
@@ -1,44 +1,44 @@
-import {useContext} from 'react'
-import {AppContext} from '../../global/app-context'
+// import {useContext} from 'react'
+// import {AppContext} from '../../global/app-context'
 import IdeaModel from '../../models/idea-model'
 
-const appContext = useContext(AppContext)
+// const appContext = useContext(AppContext)
 
 export const Ideas = [
   new IdeaModel(
     '1',
     'I1',
     'test1',
-    appContext.userData.id
+    'appContext.userData.id'
   ),
   new IdeaModel(
     '2',
     'I2',
     'test2',
-    appContext.userData.id
+    'appContext.userData.id'
   ),
   new IdeaModel(
     '3',
     'I3',
     'test3',
-    appContext.userData.id
+    'appContext.userData.id'
   ),
   new IdeaModel(
     '4',
     'I4',
     'test4',
-    appContext.userData.id
+    'appContext.userData.id'
   ),
   new IdeaModel(
     '5',
     'I5',
     'test5',
-    appContext.userData.id
+    'appContext.userData.id'
   ),
   new IdeaModel(
     '6',
     'I6',
     'test6',
-    appContext.userData.id
+    'appContext.userData.id'
   )
 ]
