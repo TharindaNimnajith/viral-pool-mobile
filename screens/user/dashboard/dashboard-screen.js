332127dcf7c8fd61fbdfb8c6ac42861f0a988d98
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-29 04:33:57 +0530

    edit profile

--- screens/user/dashboard/dashboard-screen.js
+++ screens/user/dashboard/dashboard-screen.js
@@ -1,5 +1,6 @@
 import React, {useContext, useEffect} from 'react'
 import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native'
+import {heightPercentageToDP as hp, widthPercentageToDP as wp} from 'react-native-responsive-screen'
 import axios from 'axios'
 import {AppContext} from '../../../global/app-context'
 import Colors from '../../../shared/colors'
