6186ed84431d48ec6d35da08940c65e33b2b2c95
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-30 18:00:55 +0530

    ui

--- screens/user/ideas/idea-list-screen.js
+++ screens/user/ideas/idea-list-screen.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import {FlatList, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native'
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen'
 import Colors from '../../../shared/colors'
 import {Ideas} from '../../../data/idea-data/idea-data'
@@ -31,26 +31,22 @@ const IdeaListScreen = ({navigation}) => {
   }
 
   return (
-    <SafeAreaView>
-      <View style={styles.mainViewStyle}>
-        {/*<ScrollView>*/}
-          <View style={styles.listStyle}>
-            <View style={styles.viewStyle}>
-              <TouchableOpacity style={styles.buttonStyle}
-                                onPress={redirectToAddIdeaScreen}>
-                <Text style={styles.buttonTextStyle}>
-                  New Idea
-                </Text>
-              </TouchableOpacity>
-            </View>
-            <FlatList keyExtractor={(item, index) => index.toString()}
-                      data={Ideas}
-                      numColumns={1}
-                      renderItem={renderItemsFunction}/>
-          </View>
-        {/*</ScrollView>*/}
+    <View style={styles.mainViewStyle}>
+      <View style={styles.viewStyle}>
+        <TouchableOpacity style={styles.buttonStyle}
+                          onPress={redirectToAddIdeaScreen}>
+          <Text style={styles.buttonTextStyle}>
+            New Idea
+          </Text>
+        </TouchableOpacity>
       </View>
-    </SafeAreaView>
+      <View style={styles.listStyle}>
+        <FlatList keyExtractor={(item, index) => index.toString()}
+                  data={Ideas}
+                  numColumns={1}
+                  renderItem={renderItemsFunction}/>
+      </View>
+    </View>
   )
 }
 
@@ -58,25 +54,27 @@ const styles = StyleSheet.create({
   buttonStyle: {
     marginTop: 20,
     marginBottom: 10,
-    marginRight: 15,
     backgroundColor: Colors.primaryColor,
-    alignItems: 'center',
     padding: 10,
     width: wp('40%'),
-    borderRadius: 5
+    borderRadius: 5,
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    alignItems: 'center'
   },
   buttonTextStyle: {
     color: Colors.secondaryColor,
     textTransform: 'uppercase'
   },
   listStyle: {
+    marginTop: 70,
     width: wp('100%')
   },
   mainViewStyle: {
     backgroundColor: Colors.secondaryColor
   },
   viewStyle: {
-    alignItems: 'flex-end'
+    marginRight: 15
   }
 })
 
