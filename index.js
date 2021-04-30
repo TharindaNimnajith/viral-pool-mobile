1eb5760e1d9b4f26a7c5629b0f1a8564f0c2679b
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-05-01 03:04:29 +0530

    ui

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import registerRootComponent from 'expo/build/launch/registerRootComponent'
+// import {useContext} from 'react'
 import axios from 'axios'
+// import {AppContext} from './global/app-context'
 import {Util} from './util/util'
 import {getStringData} from './helpers/local-storage-helpers'
 import App from './App'
 
-const accessToken = getStringData(Util.ACCESS_TOKEN)
+// const accessToken = getStringData(Util.ACCESS_TOKEN)
 
 axios.defaults.baseURL = Util.BASE_URL
 
@@ -12,6 +14,9 @@ axios.defaults.baseURL = Util.BASE_URL
 // axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 axios.interceptors.request.use(async request => {
+  // const appContext = useContext(AppContext)
+  // const accessToken = appContext.accessToken
+  const accessToken = await getStringData(Util.ACCESS_TOKEN)
   request.headers['Authorization'] = `Bearer ${accessToken}`
   request.headers['client_id'] = 'UFwv4s5sAHYyRS2q'
   // console.log(request)
