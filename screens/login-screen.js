f8d4226fe44e1d3f94dbeeb7aaded1d993e2a834
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-05-17 23:26:25 +0530

    dev

--- screens/login-screen.js
+++ screens/login-screen.js
@@ -23,10 +23,10 @@ import {storeStringData} from '../util/local-storage'
 const LoginScreen = props => {
   const appContext = useContext(AppContext)
 
-  const [email, setEmail] = useState('tharindarajapakshe@y7mail.com')
-  const [password, setPassword] = useState('tharinda')
-  const [emailValid, setEmailValid] = useState(true)
-  const [passwordValid, setPasswordValid] = useState(true)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [emailValid, setEmailValid] = useState(false)
+  const [passwordValid, setPasswordValid] = useState(false)
   const [loading, setLoading] = useState(false)
   const [refreshing, setRefreshing] = useState(false)
 
@@ -46,7 +46,7 @@ const LoginScreen = props => {
   }
 
   function isDisabled() {
-    return !emailValid || !passwordValid
+    return !emailValid || !passwordValid || true
   }
 
   const login = async () => {
